54d946712c3799b01dca48483ecadc9b20eee1f0
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-02 15:27:21 +0300

    for & for_in_for

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,12 +1,14 @@
-﻿//Вид 1
+﻿/*Вид 1
+
 void Metod1()
 {
     System.Console.WriteLine("Автор Григоренко");
 }
-/*Metod1();*/
+Metod1();*/
 
 
-//Вид2
+/*Вид2
+
 void Metod2(string msg)
 {
     System.Console.WriteLine(msg);
@@ -22,7 +24,7 @@ void Metod21(string msg, int count)
         i++;
     }
 }
-//Metod21("Текст", 4);
+Metod21("Текст", 4);*/
 
 
 
@@ -39,7 +41,7 @@ System.Console.WriteLine(year);*/
 
 //Вид4
 
-string Metod4(int count, string text)
+/*string Metod4(int count, string text)
 {
     int i =0;
     string result = String.Empty;
@@ -52,4 +54,31 @@ string Metod4(int count, string text)
 }
 
 string res = Metod4(10, "u");
-System.Console.WriteLine(res);
+System.Console.WriteLine(res);*/
+
+
+/* for
+
+string Metod5(int count, string text)
+{
+    string result = String.Empty;
+    for (int i =0; i<count; i++)
+    {
+        result = result +text;
+    }
+    return result;
+}
+
+string res = Metod5(10, "u");
+System.Console.WriteLine(res);*/
+
+// Цикл в Цикле
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 2; j <=10; j++)
+    {
+        System.Console.WriteLine($"{i}*{j}={i*j}");
+    }
+    System.Console.WriteLine();
+}
